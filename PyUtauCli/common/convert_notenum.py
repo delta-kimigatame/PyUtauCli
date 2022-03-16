94b3279e05783a049ff1a973952a618e46d3a844
delta_kimigatame <sankaku.kuro@gmail.com>
2022-03-16 21:53:50 +0900

    整形

--- PyUtauCli/common/convert_notenum.py
+++ PyUtauCli/common/convert_notenum.py
@@ -3,19 +3,20 @@
 '''
 from typing import Literal
 
-TONE_NUM: dict = {"C":0, "C#":1, "C♯":1, "Db":1, "D♭":1,
-                  "D":2, "D#":3, "D♯":3, "Eb":3, "E♭":3,
-                  "E":4, 
-                  "F":5, "F#":6, "F♯":6, "Gb":6, "G♭":6,
-                  "G":7, "G#":8, "G♯":8, "Ab":8, "A♭":8,
-                  "A":9, "A#":10, "A♯":10, "Bb":10, "B♭":10,
-                  "B":11}
+TONE_NUM: dict = {"C": 0, "C#": 1, "C♯": 1, "Db": 1, "D♭": 1,
+                  "D": 2, "D#": 3, "D♯": 3, "Eb": 3, "E♭": 3,
+                  "E": 4,
+                  "F": 5, "F#": 6, "F♯": 6, "Gb": 6, "G♭": 6,
+                  "G": 7, "G#": 8, "G♯": 8, "Ab": 8, "A♭": 8,
+                  "A": 9, "A#": 10, "A♯": 10, "Bb": 10, "B♭": 10,
+                  "B": 11}
 
-TONE_NAME: dict = {"#":["C","C#","D","D#","E","F","F#","G","G#","A","A#","B"],
-                   "♯":["C","C♯","D","D♯","E","F","F♯","G","G♯","A","A♯","B"],
-                   "b":["C","Db","D","Eb","E","F","Gb","G","Ab","A","Bb","B"],
-                   "♭":["C","D♭","D","E♭","E","F","G♭","G","A♭","A","B♭","B"],
+TONE_NAME: dict = {"#": ["C", "C#", "D", "D#", "E", "F", "F#", "G", "G#", "A", "A#", "B"],
+                   "♯": ["C", "C♯", "D", "D♯", "E", "F", "F♯", "G", "G♯", "A", "A♯", "B"],
+                   "b": ["C", "Db", "D", "Eb", "E", "F", "Gb", "G", "Ab", "A", "Bb", "B"],
+                   "♭": ["C", "D♭", "D", "E♭", "E", "F", "G♭", "G", "A♭", "A", "B♭", "B"],
                    }
+
 
 def toInt(tone: str) -> int:
     '''
@@ -38,11 +39,12 @@ def toInt(tone: str) -> int:
         音名以外の文字列が与えられたとき
     '''
     try:
-        return TONE_NUM[tone[:-1]] + (int(tone[-1])+1) * 12
+        return TONE_NUM[tone[:-1]] + (int(tone[-1]) + 1) * 12
     except:
         raise ValueError("{} is not tone-name.".format(tone))
 
-def toStr(notenum: int, mark: Literal["#","b","＃","♭"] = "#") -> str:
+
+def toStr(notenum: int, mark: Literal["#", "b", "＃", "♭"]="#") -> str:
     '''
     notenumを音名に変換します。
 
@@ -66,6 +68,6 @@ def toStr(notenum: int, mark: Literal["#","b","＃","♭"] = "#") -> str:
 
     '''
     try:
-        return TONE_NAME[mark][notenum % 12] + str(notenum // 12 -1)
+        return TONE_NAME[mark][notenum % 12] + str(notenum // 12 - 1)
     except:
         raise ValueError("{} is not sharp or flat.[#,b,♯,♭]".format(mark))
